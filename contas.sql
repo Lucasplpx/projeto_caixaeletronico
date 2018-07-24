46e9d03384df773dd173ddfb140ff45ecb81c22f
Lucas Passos <lucasplpx@gmail.com>
2018-07-23 21:05:03 -0300

    Add bd

---
+++ contas.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 24-Jul-2018 às 02:03
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `projeto_caixaeletronico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contas`
+--
+
+CREATE TABLE `contas` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `titular` varchar(100) DEFAULT NULL,
+  `agencia` int(11) DEFAULT NULL,
+  `conta` int(11) DEFAULT NULL,
+  `senha` varchar(32) DEFAULT NULL,
+  `saldo` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `contas`
+--
+
+INSERT INTO `contas` (`id`, `titular`, `agencia`, `conta`, `senha`, `saldo`) VALUES
+(1, 'Lucas Passos', 123, 321, '202cb962ac59075b964b07152d234b70', 5083.2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contas`
+--
+ALTER TABLE `contas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contas`
+--
+ALTER TABLE `contas`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
